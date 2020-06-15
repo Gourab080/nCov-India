87d8ee7684dfc3e79d2d5681ee9aad9595dc50e2
Gourab080 <66375192+Gourab080@users.noreply.github.com>
2020-06-15 15:57:44 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,39 +1,135 @@
-import React from 'react';
-import * as Icon from 'react-feather';
+import React from "react";
+import * as Icon from "react-feather";
+// import ReactGa from "react-ga";
 
-function Footer(props) {
+const Footer = () => {
   return (
-    <footer className="fadeInUp" style={{animationDelay: '2s'}}>
-        {/* <img
-          src="/icon.png"
-          alt="https://www.pandemic2020.in | Coronavirus cases live Updates India"
-        />*/}
-
-        <h5>We stand with everyone fighting on the frontlines ♥️</h5>
-        <div className="link">
-          <a href="https://pandemic2020.in">Pandemic 2020</a>
+    <div
+      className="fadeInUp"
+      style={{ animationDelay: "3s", marginTop: "50px" }}
+    >
+      <h6
+        style={{
+          textAlign: "center",
+          marginBottom: 25,
+        }}
+      >
+        Salute to the ones fighting on the frontlines
+        <a
+          href="https://twitter.com/covidindiastats"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h6
+            style={{
+              textAlign: "center",
+              marginTop: 5,
+            }}
+          >
+            <span className="hashTag">#DistancedButNotAlone</span>
+          </h6>
+        </a>
+      </h6>
+      <div className="row">
+        <div align="center" className="col">
+          <a
+            href="https://t.me/covidindiastats"
+            className="button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "white",
+                backgroundColor: "rgb(0, 136, 204)",
+                marginTop: 7.5,
+                width: 250,
+                fontWeight: 650,
+                fontSize: 15,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Telegram Clicked",
+                  action: "Telegram",
+                })
+              }
+            >
+              <Icon.Inbox />
+              &ensp; JOIN US ON TELEGRAM
+            </button>
+          </a>
         </div>
-       
-        <a
-          className="button excel"
-          href="https://Pandemic2020.in"
-          target="_noblank"
-        >
-          <Icon.Code />
-          <span>Made By Gourab</span>
-        </a>
-        
-        <a
-          href="https://ko-fi.com/gourabdas"
-          className="button telegram"
-          target="_noblank"
-        >
-          <Icon.Coffee />
-          <span>Buy Me A Coffee</span>
-        </a>
-
-      </footer>
+        <div align="center" className="col">
+          <a
+            href="https://facebook.com/covidindiastats"
+            className="button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "rgb(59, 89, 152)",
+                backgroundColor: "rgba(59, 89, 152, 0.4)",
+                marginTop: 7.5,
+                width: 250,
+                fontWeight: 800,
+                fontSize: 15,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Facebook button Clicked",
+                  action: "Facebook",
+                })
+              }
+            >
+              <Icon.Facebook />
+              &ensp; LIKE ON FACEBOOK
+            </button>
+          </a>
+        </div>
+        <div align="center" className="col">
+          <a
+            href="https://twitter.com/covidindiastats"
+            className="button"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <button
+              className="btn"
+              style={{
+                textAlign: "center",
+                color: "#4ea2be",
+                backgroundColor: "#d9ecf5",
+                marginTop: 8,
+                width: 250,
+                fontWeight: 700,
+                fontSize: 15,
+                alignContent: "center",
+                fontFamily: "notosans",
+              }}
+              onClick={() =>
+                ReactGa.event({
+                  category: "Twitter button Clicked",
+                  action: "Twitter",
+                })
+              }
+            >
+              <Icon.Twitter />
+              &ensp; UPDATES ON TWITTER
+            </button>
+          </a>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Footer;
