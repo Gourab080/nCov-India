26c20f2791f05b8def739b7861a2bc6d9866836d
Gourab080 <66375192+Gourab080@users.noreply.github.com>
2020-06-15 17:42:44 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {NavLink} from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 
 const navLinkProps = (path, animationDelay) => ({
   className: `fadeInUp ${window.location.pathname === path ? 'focused' : ''}`,
@@ -12,62 +13,27 @@ function Navbar({pages}) {
   const [expand, setExpand] = useState(false);
 
   return (
-    <div
-      className="Navbar"
-      style={{width: window.innerWidth > 769 && expand ? '6rem' : ''}}
-    >
-      <div className="navbar-left">English</div>
-      <div className="navbar-middle">
-        <NavLink to="/">
-          Pandemic<span>2020</span>
-        </NavLink>
-      </div>
-      <div
-        className="navbar-right"
-        style={{
-          background: expand ? '#4c75f2' : '',
-          color: expand ? 'white' : '',
-        }}
-        onClick={() => {
-          setExpand(!expand);
-        }}
-      >
-        {expand ? 'Close' : 'Menu'}
-      </div>
-      {expand && (
-        <div
-          className="expand"
-          style={{left: window.innerWidth > 769 && expand ? '6rem' : ''}}
-        >
-          {pages.map((page, i) => {
-            if (page.showInNavbar === true) {
-              return (
-                <NavLink
-                  to={page.pageLink}
-                  key={i}
-                  onClick={() => {
-                    setExpand(false);
-                  }}
-                >
-                  <span
-                    {...navLinkProps(
-                      page.pageLink,
-                      page.animationDelayForNavbar
-                    )}
-                  >
-                    {page.displayName}
-                  </span>
-                </NavLink>
-              );
-            }
-            return null;
-          })}
-          <div className="expand-bottom">
-            <h5>A crowdsourced initiative.</h5>
-          </div>
-        </div>
-      )}
-    </div>
+    <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Link</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
   );
 }
 
