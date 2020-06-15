d74cf51aec2b19a1aee4554ee571e1a4d0354c42
Gourab080 <66375192+Gourab080@users.noreply.github.com>
2020-06-15 14:22:56 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -502,9 +502,9 @@ return (
        <div className="w-full fade-in mb-4" style={animationDelay(1)}>
             <DisplayCard ref={childRef} cards={displayCards} count={2000} />
        </div>  
-      {IS_SINGLE_COLUMN && getMapAndTable()}      
+      {IS_SINGLE_COLUMN && getMapAndTable()}  
+     <Footer />    
     </div>
-    <Footer />
     )}
   </div>
 </React.Fragment>
