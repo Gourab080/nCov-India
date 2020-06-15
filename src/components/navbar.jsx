02bcdfb8ff2a032aee62f4bc7160d29e861ee819
Gourab080 <66375192+Gourab080@users.noreply.github.com>
2020-06-15 18:33:26 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,7 +12,7 @@ function Navbar({pages}) {
     const [expand, setExpand] = useState(false);
     return (
         <nav className="navbar navbar-expand-lg navbar-dark info-color">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">PANDEMIC2020</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
@@ -22,25 +22,13 @@ function Navbar({pages}) {
               <a className="nav-link waves-effect waves-light" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link waves-effect waves-light" href="#">Features</a>
+              <a className="nav-link waves-effect waves-light" href="#">Help</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link waves-effect waves-light" href="#">Pricing</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div className="dropdown-menu dropdown-info" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item waves-effect waves-light" href="#">Action</a>
-                <a className="dropdown-item waves-effect waves-light" href="#">Another action</a>
-                <a className="dropdown-item waves-effect waves-light" href="#">Something else here</a>
-              </div>
+              <a className="nav-link waves-effect waves-light" href="#">FAQs</a>
             </li>
           </ul>
-          <form className="form-inline">
-            <div className="md-form my-0">
-              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-            </div>
-          </form>
+          
         </div>
       </nav>
     );
