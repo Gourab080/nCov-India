29a4b537ff3b3c6fde8fbd42f57ae0f632571569
Gourab080 <66375192+Gourab080@users.noreply.github.com>
2020-06-15 17:24:31 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,65 +1,74 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, {useState} from 'react';
+import {NavLink} from 'react-router-dom';
 
 const navLinkProps = (path, animationDelay) => ({
-    className: `${window.location.pathname === path ? 'focused' : ''}`,
-    style: {
-        animationDelay: `${animationDelay}s`,
-    },
+  className: `fadeInUp ${window.location.pathname === path ? 'focused' : ''}`,
+  style: {
+    animationDelay: `${animationDelay}s`,
+  },
 });
 
 function Navbar({pages}) {
-    const [expand, setExpand] = useState(false);
-    return (
-        <nav className="flex items-center flex-wrap bg-white text-primary py-2">
-            <div className="container py-2 px-2 items-center font-bold text-lg">
-                <div className="w-full flex items-center justify-between -my-2">
-                    <NavLink exact={true} to='/' className="flex items-center cursor-pointer lg:flex">
-                        <img
-                            src="/images/covid.svg"
-                            alt="logo"
-                            className="h-10 w-auto mr-3"
-                        />
-                        <div className="text-xl font-bold text-black text-primary">
-                            COVID-19 India
-                            <span className="text-red-600 uppercase live-txt blink">
-                                Live
-                            </span>
-                        </div>
-                    </NavLink>
-                    <div className="flex ml-auto mr-1">
-                        {pages.map((page, i) => {
-                            if (page.showInNavbar === true) {
-                                return (
-                                    <NavLink
-                                        exact={true}
-                                        to={page.pageLink}
-                                        key={i}
-                                        className={
-                                            'mx-1 px-3 py-4 hidden lg:block nav-link'
-                                        }
-                                        activeClassName={
-                                            'relative nav-link-active'
-                                        }
-                                    >
-                                        <span
-                                            {...navLinkProps(
-                                                page.pageLink,
-                                                page.animationDelayForNavbar
-                                            )}
-                                        >
-                                            {page.displayName}
-                                        </span>
-                                    </NavLink>
-                                );
-                            }
-                            return null;
-                        })}
-                    </div>
-                </div>
-            </div>
-        </nav>
-    );
+  const [expand, setExpand] = useState(false);
+
+  return (
+    <div
+      className="Navbar"
+      style={{width: window.innerWidth > 769 && expand ? '6rem' : ''}}
+    >
+      <div className="navbar-left">English</div>
+      <div className="navbar-middle">
+        <NavLink to="/">
+          Pandemic<span>2020</span>
+        </NavLink>
+      </div>
+      <div
+        className="navbar-right"
+        style={{
+          background: expand ? '#4c75f2' : '',
+          color: expand ? 'white' : '',
+        }}
+        onClick={() => {
+          setExpand(!expand);
+        }}
+      >
+        {expand ? 'Close' : 'Menu'}
+      </div>
+      {expand && (
+        <div
+          className="expand"
+          style={{left: window.innerWidth > 769 && expand ? '6rem' : ''}}
+        >
+          {pages.map((page, i) => {
+            if (page.showInNavbar === true) {
+              return (
+                <Link
+                  to={page.pageLink}
+                  key={i}
+                  onClick={() => {
+                    setExpand(false);
+                  }}
+                >
+                  <span
+                    {...navLinkProps(
+                      page.pageLink,
+                      page.animationDelayForNavbar
+                    )}
+                  >
+                    {page.displayName}
+                  </span>
+                </Link>
+              );
+            }
+            return null;
+          })}
+          <div className="expand-bottom">
+            <h5>A crowdsourced initiative.</h5>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Navbar;
